477d1d1c3ea1c277b43ed0558c6a5e3d35172452
Farahnosh-Yousofi <80102585+Farahnosh-Yousofi@users.noreply.github.com>
2024-07-13 19:54:11 -0400

    Axios link code added and api key and api url added to app.js
    functions created to search the inputed city using api and find the weather for that city

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
+function weatherApi(response) {
+  console.log(response.data.temperature.current);
+  let temperature = response.data.temperature.current;
+  let temp = document.querySelector(".temp");
+  temp.innerHTML = ` ${Math.round(temperature)}`;
+}
+
+function cityWeather(city) {
+  let apiKey = "8e33cb0do4fat4cdc645a4c9297b9a5b";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(weatherApi);
+  console.log(apiUrl);
+}
+
 function cityName(event) {
   event.preventDefault();
   let city = document.querySelector("#city");
   let h1City = document.querySelector("h1");
   h1City.textContent = city.value;
+  cityWeather(city.value);
 }
 
 let formInput = document.querySelector("#input");
