c6f53bce21b51c595ecca3c5f25eb7740e79b5cc
Farahnosh-Yousofi <80102585+Farahnosh-Yousofi@users.noreply.github.com>
2024-07-13 20:34:50 -0400

    added current time and date

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
 function weatherApi(response) {
   console.log(response.data.temperature.current);
+
   let temperature = response.data.temperature.current;
+  let condition = response.data.condition.description;
+  let humidity = response.data.temperature.humidity;
+  let windSpeed = response.data.wind.speed;
+
   let temp = document.querySelector(".temp");
+  let descriptionElement = document.querySelector(".description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   temp.innerHTML = ` ${Math.round(temperature)}`;
+  descriptionElement.innerHTML = condition;
+  humidityElement.innerHTML = humidity;
+  windElement.innerHTML = windSpeed;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function cityWeather(city) {
